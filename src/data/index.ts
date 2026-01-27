e3dfcdb26a9f6d7dac8f230ba30c23d77adbc129
krasavushkin <artemkrasavushkin@gmail.com>
2026-01-27 16:18:20 +0300

    1. deleted unused hook

--- src/data/index.ts
+++ src/data/index.ts
@@ -30,23 +30,6 @@ export const ALL_CABLES = [
     ...SCAB_M,
     ...SCAB_S,
 ];
-export const cableLoaders: Record<string, () => Promise<any[]>> = {
-    SCAB: () => import("./cables/SCAB.json"),
-    SCAB_M: () => import("./cables/SCAB_M.json"),
-/*
-    SCAB_S: () => import("./cables/SCAB_S.json"),
-*/
-    KPS: () => import("./cables/KPS.json"),
-    KSB: ()=> import("./cables/KSB.json"),
-    KSBG: ()=> import("./cables/KSBG.json"),
-    KUNRS: ()=> import("./cables/KUNRS.json"),
-    KUNRS_LTX: ()=> import("./cables/KUNRS_LTX.json"),
-    LAN: ()=> import("./cables/LAN.json"),
-    LTX: ()=> import("./cables/LTX.json"),
-    KERS: () => import("./cables/KERS.json"),
-    KSB_LTX: () => import("./cables/KSB_LTX.json"),
-    KSHS: () => import("./cables/KSHS.json"),
-};
 
 
 export { OKL_DB, OKL_CABLE_MAP };
